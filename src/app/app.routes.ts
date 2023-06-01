afd4aa673c14785dfade5f765ff983cf2a84107a
Brian McGowan <brian@nextdaylost.com>
2023-06-01 18:02:41 -0400

    Add stacked layout stub

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { StackedLayoutComponent } from '@app/layouts/stacked-layout/stacked-layout.component';
+
+export const routes: Routes = [{ path: '', component: StackedLayoutComponent }];
---
+++ src/app/layouts/stacked-layout/stacked-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-stacked-layout',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './stacked-layout.component.html',
+  styleUrls: ['./stacked-layout.component.css'],
+})
+export class StackedLayoutComponent {}
