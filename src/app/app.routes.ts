bffa21da075ac7d1d4c0b4a718bf84dca1b62234
Brian McGowan <brian@nextdaylost.com>
2023-06-02 00:21:34 -0400

    Add placeholder components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,19 @@
 import { Routes } from '@angular/router';
 
+import { GridComponent } from '@app/components/grid/grid.component';
+import { HomeComponent } from '@app/components/home/home.component';
+import { MapComponent } from '@app/components/map/map.component';
 import { StackedLayoutComponent } from '@app/layouts/stacked-layout/stacked-layout.component';
 
-export const routes: Routes = [{ path: '', component: StackedLayoutComponent }];
+export const routes: Routes = [
+  {
+    path: '',
+    component: StackedLayoutComponent,
+    children: [
+      { path: 'grid', component: GridComponent },
+      { path: 'home', component: HomeComponent },
+      { path: 'map', component: MapComponent },
+      { path: '', pathMatch: 'full', redirectTo: 'home' },
+    ],
+  },
+];
--- src/app/layouts/stacked-layout/stacked-layout.component.ts
+++ src/app/layouts/stacked-layout/stacked-layout.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NavComponent } from '@app/components/nav/nav.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-stacked-layout',
   standalone: true,
-  imports: [CommonModule, NavComponent],
+  imports: [CommonModule, NavComponent, RouterOutlet],
   templateUrl: './stacked-layout.component.html',
   styleUrls: ['./stacked-layout.component.css'],
 })
