a6a68393f02bbf8065fc794d63c3aa52d354d989
Brian McGowan <brian@nextdaylost.com>
2023-06-05 13:47:00 -0400

    Add map component

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { latLng, MapOptions, tileLayer } from 'leaflet';
+
+import { environment } from '@environment';
+
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LeafletModule],
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css'],
+  host: { class: 'flex flex-col flex-auto' },
 })
-export class MapComponent {}
+export class MapComponent {
+  mapOptions: MapOptions = {
+    center: latLng(
+      environment.map.options.center.latitude,
+      environment.map.options.center.longitude
+    ),
+    layers: environment.map.options.layers.map((layer) => {
+      return tileLayer(layer.urlTemplate, layer.options);
+    }),
+    zoom: environment.map.options.zoom,
+  };
+}
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -5,4 +5,23 @@ export const environment = {
   appInfo: {
     support: pkg.author,
   },
+  map: {
+    options: {
+      center: {
+        latitude: 38.9072,
+        longitude: -77.0369,
+      },
+      layers: [
+        {
+          urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          options: {
+            attribution: 'OpenStreetMap',
+            maxZoom: 18,
+            minZoom: 5,
+          },
+        },
+      ],
+      zoom: 12,
+    },
+  },
 };
