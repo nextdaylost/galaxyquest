948c53293e52aecb0efd701436297c235ffdb5e4
Brian McGowan <brian@nextdaylost.com>
2023-06-05 08:10:47 -0400

    Add footer component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from '@app/components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FooterComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
+  host: { class: 'flex flex-col flex-auto' },
 })
 export class HomeComponent {}
