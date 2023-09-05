1e810a97564043d1b5d58cf46ab9d9c844405e5f
Brian McGowan <brian@nextdaylost.com>
2023-09-05 05:52:16 -0400

    Add data grid with mock data

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AgGridModule } from 'ag-grid-angular';
-import { ColDef, GridApi, GridReadyEvent } from 'ag-grid-community';
+import {
+  ColDef,
+  DataTypeDefinition,
+  GridApi,
+  GridReadyEvent,
+  ValueFormatterLiteParams,
+  ValueParserLiteParams,
+} from 'ag-grid-community';
+import 'ag-grid-enterprise';
 
-import { GeoPoint } from '@app/interfaces/geopoint';
+import { ITreadData } from '@app/interfaces/datasets/ITreadData';
 
 @Component({
   selector: 'app-grid',
@@ -12,24 +20,28 @@ import { GeoPoint } from '@app/interfaces/geopoint';
   imports: [CommonModule, AgGridModule, HttpClientModule],
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.css'],
+  host: { class: 'flex flex-col flex-auto' },
 })
 export class GridComponent {
-  private gridApi: GridApi<GeoPoint>;
+  private gridApi: GridApi<ITreadData>;
 
   columnDefs: ColDef[] = [
-    { field: 'format', cellDataType: 'text', headerName: 'Log Format' },
-    { field: 'utc', cellDataType: 'text', headerName: 'Timestamp' },
+    { field: 'id', cellDataType: 'text', headerName: 'Id' },
+    {
+      field: 'created_at',
+      cellDataType: 'dateString',
+      headerName: 'Created At',
+    },
+    { field: 'timestamp', cellDataType: 'dateString', headerName: 'Timestamp' },
     { field: 'lat', cellDataType: 'number', headerName: 'Latitude' },
     { field: 'lon', cellDataType: 'number', headerName: 'Longitude' },
     { field: 'quality', cellDataType: 'number', headerName: 'Quality' },
-    { field: 'num_sats', cellDataType: 'number', headerName: 'Satellites' },
+    { field: 'num_sats', cellDataType: 'number', headerName: 'No. Satellites' },
     { field: 'hdop', cellDataType: 'number', headerName: 'HDoP' },
     { field: 'alt', cellDataType: 'number', headerName: 'Altitude' },
-    { field: 'a_units', cellDataType: 'text', headerName: 'Alt. Units' },
     { field: 'undulation', cellDataType: 'number', headerName: 'Undulation' },
-    { field: 'u_units', cellDataType: 'text', headerName: 'Und. Units' },
     { field: 'age', cellDataType: 'number', headerName: 'Age' },
-    { field: 'station_id', cellDataType: 'text', headerName: 'Station Id' },
+    { field: '_source', cellDataType: 'text', headerName: 'Source File' },
   ];
 
   defaultColDef: ColDef = {
@@ -38,31 +50,67 @@ export class GridComponent {
     sortable: true,
   };
 
+  // TODO: Fix dataString type definition. Timestamp is being rendered, but is not
+  //  parsed correctly/filterable.
+  dataTypeDefinitions: {
+    [cellDataType: string]: DataTypeDefinition;
+  } = {
+    dateString: {
+      baseDataType: 'dateString',
+      extendsDataType: 'dateString',
+      valueParser: (params: ValueParserLiteParams<ITreadData, string>) => {
+        return params.newValue != null &&
+          params.newValue.match(
+            '(\\d{4})-(\\d{2})-(\\d{2})T(\\d{2}):(\\d{2}):(\\d{2})(\\.\\d+)?(\\+\\d{2}:\\d{2})?'
+          )
+          ? params.newValue
+          : null;
+      },
+      valueFormatter: (
+        params: ValueFormatterLiteParams<ITreadData, string>
+      ) => {
+        return params.value == null ? '' : params.value;
+      },
+      dataTypeMatcher: (value: any) => {
+        return (
+          typeof value === 'string' &&
+          !!value.match(
+            '(\\d{4})-(\\d{2})-(\\d{2})T(\\d{2}):(\\d{2}):(\\d{2})(\\.\\d+)?(\\+\\d{2}:\\d{2})?'
+          )
+        );
+      },
+      dateParser: (value: string | undefined) => {
+        if (value == null || value === '') {
+          return undefined;
+        }
+        // TODO: Maybe check format to confirm ISO 8601 compliance and return null
+        //  if not correct.
+        return new Date(value);
+      },
+      dateFormatter: (value: Date | undefined) => {
+        if (value == null) {
+          return undefined;
+        }
+        const date = String(value.getDate());
+        const month = String(value.getMonth() + 1);
+        return `${date.length === 1 ? '0' + date : date}/${
+          month.length === 1 ? '0' + month : month
+        }/${value.getFullYear()}`;
+      },
+    },
+  };
+
   rowData: any[] = [];
 
   constructor(private http: HttpClient) {}
 
-  onGridReady(params: GridReadyEvent<GeoPoint>) {
+  onGridReady(params: GridReadyEvent<ITreadData>) {
     this.gridApi = params.api;
-    this.gridApi.sizeColumnsToFit({
-      defaultMinWidth: 100,
-      columnLimits: [
-        { key: 'format', minWidth: 130 },
-        { key: 'utc', minWidth: 200 },
-        { key: 'lat', minWidth: 120 },
-        { key: 'lon', minWidth: 120 },
-        { key: 'num_sats', minWidth: 120 },
-        { key: 'alt', minWidth: 120 },
-        { key: 'a_units', minWidth: 120 },
-        { key: 'undulation', minWidth: 130 },
-        { key: 'u_units', minWidth: 120 },
-        { key: 'station_id', minWidth: 120 },
-      ],
-    });
+    this.gridApi.sizeColumnsToFit();
 
     // TODO: Move this to a component.
     this.http
-      .get<{ data: GeoPoint[] }>('/assets/mock/data.json')
+      .get<{ data: ITreadData[] }>('/assets/mock/data.json')
       .subscribe((resp) => {
         this.rowData = resp.data;
       });
---
+++ src/app/interfaces/datasets/ITreadData.ts
@@ -0,0 +1,14 @@
+export interface ITreadData {
+  id: string;
+  created_at: string;
+  timestamp: string;
+  lat: number;
+  lon: number;
+  quality: number;
+  num_sats: number;
+  hdop: number;
+  alt: number;
+  undulation: number;
+  age: number;
+  _source: number;
+}
